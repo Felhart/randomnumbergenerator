45e415bdd4027a12b7bcdd32a047b9dfa38b0e7b
Felhart <aksumehmet816178@gmail.com>
2025-10-04 11:18:48 +0300

    a

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from PyQt5.QtWidgets import QApplication
 from ui.main_window import MainWindow
 
 if __name__ == '__main__':
-    # Uygulama nesnesini oluştur
+    # Uygulama nesnkesini oluştur
     app = QApplication(sys.argv)
 
     # Ana pencereyi oluştur ve göster
